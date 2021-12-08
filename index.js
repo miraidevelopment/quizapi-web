10800ec8fe95c2f8cf68de71517680ce5a887f21
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-12-08 00:30:27 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,54 @@
+/* Fazendo a requisição do dotenv */
+require('dotenv/config');
+/* Ultilização da aoi.JS */
+const Aoijs = require("aoi.js")
+
+//Configurando a Database
+const aoifb = require("aoijs.firebase")
+const firebase = aoifb.create({
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+});
+
+//Configurando o Client da Aoi.js
+
+const bot = new Aoijs.Bot({
+    mobilePlatform: false,
+    intents: all,
+    token: process.env.TOKEN,
+    prefix: ["$getServerVar[prefixo]", "<@917962601923760139> ", "<@!917962601923760139> "],
+    database: firebase,
+    sharding: false,
+    shardAmount: 0,
+    autoUpdate: false,
+    fetchInvites: false,
+    suppressAllErrors: true,
+    debugs:{
+      interpreter:true
+    },
+    events:{
+      timeout:true,
+      functionError: true,
+      music: true
+    }
+})
+ 
+
+/* Configuração */
+
+bot.onMessage({
+    respondToBots: false,
+    guildOnly: true
+});
+bot.readyCommand({
+    channel: "",
+    code: `$log[Ligado no usuário $userTag[$clientID]]`
+})
+require('./src/datas')(bot);
+require('./questionsAPI.js')(bot)
---
+++ src/datas.js
@@ -0,0 +1,22 @@
+module.exports = (bot) => {
+    bot.variables({
+
+        prefixo: "q?",
+        version: "0.0.1",
+
+        /* Categoria 1 (Esportes)*/
+        esportesTitle: "Esportes",
+        esportesDesc: "Aqui você encontra diversas questões de esporte do Brasil e do Mundo!",
+
+
+        q1c1: "",
+        a1c1: "",
+        r1c1: "",
+
+        /* Categoria 2 (História) */
+
+        q1c1: "",
+        a1c1: "",
+        r1c1: "",
+    })
+}
