49ba7f1b3c1e92c5793b00227811e5445edfa02b
guihrib <guiguiribeiro.2020@gmail.com>
2021-12-09 11:46:50 -0300

    novo commit

--- bot.js
+++ bot.js
@@ -79,7 +79,8 @@ bot.command ({
   nonPrefixed: true,
   code:`
 $if[$getGlobalUserVar[afk;$mentioned[1]]==activated]
-$reply[$messageID;😬・$username[$mentioned[1]] está no **modo AFK**. Deu uma saidinha...> Motivo: \`\`\`$getGlobalUserVar[motivoAfk;$mentioned[1]]\`\`\`{delete:7s};yes]
+$reply[$messageID;😬・$username[$mentioned[1]] está no **modo AFK**. Deu uma saidinha...
+> Motivo: \`\`\`$getGlobalUserVar[motivoAfk;$mentioned[1]]\`\`\`{delete:7s};yes]
 $endif
 `
 })
