706a395b20a36743254661be2dde2ecc0e068e4d
guihrib <guiguiribeiro.2020@gmail.com>
2021-12-09 00:09:40 -0300

    primeira questão de esporte

--- src/datas.js
+++ src/datas.js
@@ -6,16 +6,19 @@ module.exports = (bot) => {
         bl: "no",
         color: "#2f3136",
 
-        /* Categoria 1 (Esportes)*/
-        esportesTitle: "Esportes",
+        /* Categoria 1 (Esporte)*/
+        esportesTitle: "Esporte",
         esportesDesc: "Aqui você encontra diversas questões de esporte do Brasil e do Mundo!",
 
 
-        q1c1: "",
-        a1c1: "",
-        r1c1: "",
+        q1c1: "Qual foi o maior corredor do mundo?",
+        a1c1: "1) Usain Bolt\n2) Pelé\n3) Oscar Schmidt\n4) Yan Gomes\n5) Sérgio Dutra",
+        r1c1: "1",
 
         /* Categoria 2 (História) */
+        
+        historiaTitle: "História",
+        historiaDesc: "Prestou atenção nas aulas de história? Vamos testar isso agora...",
 
         q1c1: "",
         a1c1: "",
