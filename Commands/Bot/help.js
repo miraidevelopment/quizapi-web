3c5fe772bc9e0034fa9921313c8f06e13675bf1b
guihrib <guiguiribeiro.2020@gmail.com>
2021-12-14 23:50:40 -0300

    help pronto!

--- Commands/Bot/help.js
+++ Commands/Bot/help.js
@@ -3,18 +3,22 @@ module.exports = ({
     description: "Lista de comandos do Quiz Api BOT",
     category: "Bot",
     perms: "Este comando não solicita permissões.",
-    usage: "help <sigla da categoria>",
+    usage: "help",
     aliases: ["h","ajuda","cmds","comandos","commands"],
     code: `
 $reply[$messageID;{author: Minha lista de comandos:$authorAvatar}
 {thumbnail: $userAvatar[$clientID]}
 {description: \`\`\`
-Meus comandos básicos para uso, se encontram abaixo dividos em suas devidas categorias:
+Ao total, eu tenho 11 comandos. Meus comandos básicos para uso, se encontram abaixo dividos em suas devidas categorias:
 \`\`\`
-\`💡\` __BOT__
+\`💡\` __Bot:__
+\`botinfo\`・\`ping\`・\`setprefixo\`・\`status\`・\`versão\`
 
+\`📝\` __Utilidades:__
+\`afk\`・\`avatar\`・\`commandinfo\`・\`serverinfo\`
 }
-{color: $getVar[color]}  
+{color: $getVar[color]}
+{footer: © 2021 Quiz Api. Todos os direitos reservados.:$userAvatar[$clientID]}
 ;yes]
     `
 })
