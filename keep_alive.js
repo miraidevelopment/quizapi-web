18a25ea17dfe87655a3b7d6f37381c27483f4c9c
leo <80543539+inex1stent@users.noreply.github.com>
2022-01-04 18:15:10 -0300

    arrumando o código, deixando mais limpo e pequeno

--- keep_alive.js
+++ keep_alive.js
@@ -1,4 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.listen(port, () => console.log(`Bot rodando em http://localhost:${port}`));
+const app = require('express')();
+const port = 3000 || process.env.PORT;
+app.listen(port, () => console.log(`[SERVER] Servidor rodando em http://localhost:${port}`));
