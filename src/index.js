f9a31e944d73fb2a4ac26e55160c8b5a67bce130
leo <80543539+inex1stent@users.noreply.github.com>
2022-01-04 18:10:35 -0300

    arrumando o código

--- src/index.js
+++ src/index.js
@@ -4,23 +4,14 @@ const respostas = require("./methods/respostas");
 const { initializeApp } = require("firebase/app");
 
 function create(object) {
-
   try {
-  
     const app = initializeApp(object);
-
     return {
       questoes: questoes,
       alternativas: alternativas,
       respostas: respostas
     };
-
   } catch(e) { throw new Error(e) }
-  
-  
-  
 }
 
-module.exports = {
-  create
-};
+module.exports = { create };
