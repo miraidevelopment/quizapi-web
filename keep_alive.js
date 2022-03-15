5d811abcb365b81b933da59ac3c382fc5fbfe054
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2022-03-15 16:57:11 -0300

    new concept

--- keep_alive.js
+++
@@ -1,3 +0,0 @@
-const app = require('express')();
-const port = 3000 || process.env.PORT;
-app.listen(port, () => console.log(`[SERVER] Servidor rodando em http://localhost:${port}`));
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-const questoes = require("./methods/questoes");
-const alternativas = require("./methods/alternativas");
-const respostas = require("./methods/respostas");
-const { initializeApp } = require("firebase/app");
+const express = require('express');
+const app = express();
+const Discord = require("discord.js")
+const fetch = require('node-fetch')
 
-function create(object) {
-  try {
-    const app = initializeApp(object);
-    return {
-      questoes: questoes,
-      alternativas: alternativas,
-      respostas: respostas
-    };
-  } catch(e) { throw new Error(e) }
+var client
+var bot
+
+module.exports = (bota) => {
+
+  client = bota.client
+  bot    = bota
+
 }
 
-module.exports = { create };
+// Categoria de Questões: (Esportes)
+
+app.get("/esportes", async(req, res) => {
+
+    let esportesTitle = client.variables.esportesTitle
+    let esportesDesc = client.variables.esportesDesc
+
+    res.send(`<h1>Categoria [${esportesTitle}]</h1> <h2>${esportesDesc}</h2>`)
+})
+
+app.get("/esportes/1", async(req, res) => {
+
+    let q1c1 = client.variables.q1c1
+    let a1c1 = client.variables.a1c1
+    let r1c1 = client.variables.r1c1
+
+    res.json({questao: q1c1, alternativas: a1c1, resposta: r1c1})
+})
+
+
+// Categoria de Questões: (História)
+app.get("/historia", async(req, res) => {
+
+    let historiaTitle = client.variables.historiaTitle
+    let historiaDesc = client.variables.historiaDesc
+
+    res.send(`<h1>Categoria [${historiaTitle}]</h1> <h2>${historiaDesc}</h2>`)
+})
+app.get("/historia/1", async(req, res) => {
+
+    let q1c2= client.variables.q1c2
+    let a1c2= client.variables.a1c2
+    let r1c2= client.variables.r1c2
+  
+    res.json({questao: q1c2, alternativas: a1c2, resposta: r1c2})
+})
