91807ee43fe975b5c70b969a8321d74f27ed016b
Davi Patricio <davipatricio@pm.me>
2022-03-16 21:27:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
-const firebase = require('firebase')
+const firebase = require('firebase');
+
+const { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId, measurementId } = process.env;
 
 firebase.initializeApp({
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
-})
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId,
+});
 
-const db = firebase.database()
+const db = firebase.database();
 
 app.get("/:category/:id", async (req, res) => {
 
@@ -22,13 +24,10 @@ app.get("/:category/:id", async (req, res) => {
     let data = await db.ref(`api/${category}/${id}`).once('value')
         data = data.val()
 
-    if(data) return res.json({ questao: data.questao, alternativas: data.alternativas, resposta: data.resposta })
-    if(!data) return res.json({ err: true, message: "Sorry, this category or question doesn't exist :(" })
+    if (data) return res.json({ questao: data.questao, alternativas: data.alternativas, resposta: data.resposta })
+    if (!data) return res.json({ err: true, message: "Sorry, this category or question doesn't exist :(" })
 
     res.json({ err: true, message: "error" })
+});
 
-})
-
-app.listen(process.env.PORT, () => {
-    console.log('[ QuizApi - Web ] Servidor web api online!')
-})
+app.listen(process.env.PORT, () => console.log('[ QuizApi - Web ] Servidor web api online!') });
